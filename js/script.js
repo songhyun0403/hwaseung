bbce04908e0b617de30209bfccef14c0f9d3a3d6
젤리물고기 <shn271@naver.com>
2024-05-21 16:14:13 +0900

    비주얼 영역 hover 효과 완성/반응형 완성

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 $(function () {
-  const $menu = $('.gnb > li ');
-  const $submenu = $('.submenu');
-  const duration = 300;
-
-  $menu.on('mouseenter', function () {
-    $(this).find($submenu).stop().slideDown(duration);
-
-    $(this).addClass('on');
-  });
-
-  $menu.on('mouseleave', function () {
-    $submenu.slideUp(duration);
-    $menu.removeClass('on');
+  // family site
+  $('.family-site select').on('change', function () {
+    const linkValue = $(this).val();
+    window.open(linkValue);
   });
 });
