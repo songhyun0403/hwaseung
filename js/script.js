016b9b6120546a7333cf26f8bdc1eaa7fb24a078
젤리물고기 <shn271@naver.com>
2024-05-23 15:00:12 +0900

    스크롤 관련 헤더 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(function () {
   //gnb
+  const $window = $(window);
   const $header = $('#header');
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu-wrap');
@@ -37,6 +38,23 @@ $(function () {
     $submenu.stop().fadeOut(duration);
     $banner.fadeOut(duration);
   }
+
+  // 스크롤 이벤트
+  $window.on('scroll', function () {
+    //얼마나 스크롤 되었는지 값을 구해서 저장
+    const scrollTop = $(this).scrollTop();
+
+    //비주얼 영역의 세로크기 저장
+    const visualHeight = $('.visual').outerHeight();
+    console.log(scrollTop, visualHeight);
+
+    //두 값을 비고(스크롤값이 비주얼 영역의 세로보다 크다면 = 비주얼 영역을 지난다.)
+    if (scrollTop > visualHeight) {
+      $header.addClass('w-bg');
+    } else {
+      $header.removeClass('w-bg');
+    }
+  });
 
   // family site
   $('.family-site select').on('change', function () {
