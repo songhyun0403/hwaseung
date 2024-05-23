61a5d0322512f22968d7b29a35ea5a5dfbbcbf9e
젤리물고기 <shn271@naver.com>
2024-05-23 14:35:25 +0900

    메뉴 영역 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
 $(function () {
+  //gnb
+  const $header = $('#header');
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu-wrap');
+  const $banner = $('.banner-slid');
+  const $btnMenu = $('.btn-menu');
+  const duration = 300;
+
+  //마우스가 메뉴에 들어오면 (mouseenter)
+  $menu.on('mouseenter', function () {
+    const menuIdx = $(this).index();
+    $menu.removeClass('on').eq(menuIdx).addClass('on');
+    $submenu.find('li').removeClass('on').eq(menuIdx).addClass('on');
+
+    openMenu();
+  });
+
+  //마우스가 메뉴에 나가면 (mouseleave)
+  $header.on('mouseleave', function () {
+    $menu.removeClass('on');
+    $submenu.find('li').removeClass('on');
+    closeMenu();
+  });
+
+  //메뉴 버튼을 클릭했을 때
+  $btnMenu.on('click', openMenu);
+
+  // 메뉴의 동작을 함수로 정의
+  function openMenu() {
+    $header.addClass('active');
+    $submenu.stop().fadeIn(duration);
+    $banner.fadeIn(duration);
+  }
+  function closeMenu() {
+    $header.removeClass('active');
+    $submenu.stop().fadeOut(duration);
+    $banner.fadeOut(duration);
+  }
+
   // family site
   $('.family-site select').on('change', function () {
     const linkValue = $(this).val();
