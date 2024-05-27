4aec88d292d97a60ae866fc89aa1e96fafa86e9d
젤리물고기 <shn271@naver.com>
2024-05-27 15:25:03 +0900

    lang 추가 및 스크립트 완성

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 $(function () {
-  //gnb
+  // GNB
   const $window = $(window);
   const $header = $('#header');
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu-wrap');
-  const $banner = $('.banner-slid');
+  const $banner = $('.banner-slide');
   const $btnMenu = $('.btn-menu');
   const duration = 300;
-  //모바일
+
+  // 모바일
   const $btnMmenu = $('.btn-m-menu');
-  const $msubmenu = $('.m-submenu-wrap');
+  const $mSubmenu = $('.m-submenu-wrap');
   const $dim = $('.dim');
   const $btnClose = $('.btn-close');
+  const $mGnbMenu = $('.m-gnb > li');
+  const $mGnbSubmenu = $('.m-gnb-sub');
+
+  //모바일 용 메뉴를 클릭했을 때
+  $mGnbMenu.on('click', function () {
+    $(this).toggleClass('on');
+    $(this).siblings().removeClass('on');
+    $(this).find($mGnbSubmenu).stop().slideToggle(duration);
+    $(this).siblings().find($mGnbSubmenu).stop().slideUp(duration);
+  });
 
   $btnMmenu.on('click', function () {
-    $msubmenu.addClass('active');
+    $mSubmenu.addClass('active');
     $dim.fadeIn(duration);
   });
-  $btnClose.on('click', function () {
-    $msubmenu.removeClass('active');
+
+  $btnClose.add($dim).on('click', function () {
+    $mSubmenu.removeClass('active');
     $dim.fadeOut(duration);
+
+    //모바일 용 서브메뉴 추가
+    $mGnbMenu.removeClass('on');
+    $mGnbSubmenu.stop().slideUp(duration);
   });
 
-  //마우스가 메뉴에 들어오면 (mouseenter)
+  // 마우스가 메뉴에 들어오면(mouseenter)
   $menu.on('mouseenter', function () {
     const menuIdx = $(this).index();
     $menu.removeClass('on').eq(menuIdx).addClass('on');
@@ -31,43 +47,51 @@ $(function () {
     openMenu();
   });
 
-  //마우스가 메뉴에 나가면 (mouseleave)
+  // 마우스가 메뉴에 나가면(mouseleave)
   $header.on('mouseleave', function () {
     $menu.removeClass('on');
     $submenu.find('li').removeClass('on');
     closeMenu();
   });
 
-  //메뉴 버튼을 클릭했을 때
+  // 메뉴 버튼을 클릭했을 때
   $btnMenu.on('click', openMenu);
 
   // 메뉴의 동작을 함수로 정의
   function openMenu() {
     $header.addClass('active');
     $submenu.stop().fadeIn(duration);
-    $banner.fadeIn(duration);
+    $banner.stop().fadeIn(duration);
   }
+
   function closeMenu() {
     $header.removeClass('active');
     $submenu.stop().fadeOut(duration);
-    $banner.fadeOut(duration);
+    $banner.stop().fadeOut(duration);
   }
 
-  // 스크롤 이벤트
-  $window.on('scroll', function () {
-    //얼마나 스크롤 되었는지 값을 구해서 저장
-    const scrollTop = $(this).scrollTop();
+  let scrollTop = $window.scrollTop();
+  const visualHeight = $('.visual').outerHeight();
+  setWhiteBackground();
 
-    //비주얼 영역의 세로크기 저장
-    const visualHeight = $('.visual').outerHeight();
-    console.log(scrollTop, visualHeight);
-
-    //두 값을 비고(스크롤값이 비주얼 영역의 세로보다 크다면 = 비주얼 영역을 지난다.)
-    if (scrollTop > visualHeight) {
+  function setWhiteBackground() {
+    if (scrollTop >= visualHeight) {
       $header.addClass('w-bg');
     } else {
       $header.removeClass('w-bg');
     }
+  }
+
+  // 스크롤 이벤트
+  $window.on('scroll', function () {
+    // 얼마나 스크롤 되었는지 값을 구해서 저장
+    scrollTop = $(this).scrollTop();
+    setWhiteBackground();
+  });
+
+  //언어 선택
+  $('.btn-lang').on('click', function () {
+    $('.lang-select').stop().slideToggle(duration);
   });
 
   // family site
